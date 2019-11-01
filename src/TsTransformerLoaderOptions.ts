38aa0eaade1580069f59ab9db6112d5671256ba2
alex-gusev <alex-gusev@yandex-team.ru>
2019-11-01 22:16:56 +0300

    gitpkg

---
+++ src/TsTransformerLoaderOptions.ts
@@ -0,0 +1,7 @@
+import { OptionObject } from 'loader-utils';
+import * as ts from 'typescript';
+
+export interface TsTransformerLoaderOptions extends OptionObject {
+    configName?: string;
+    getTransformers?: (program: ts.Program) => ts.TransformerFactory<ts.SourceFile>[] | string;
+}
